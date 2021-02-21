4cb25145b9663c94fc7bf155cb476ff62d33f6f9
Steve Musans <stevemuland121@gmail.com>
2021-02-20 21:29:10 -0800

    Final commit, version 1 deployed 20 feb 2021

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,18 +4,18 @@
 	$visitor_email =$_POST['email'];
 	$message = $_POST['message'];
 	
-	$email_form ='stevemuland121@gmail.com';
+	$email_form ='info.inspiration.design@gmail.com';
 	$email_subject ="Message from Inspiration-Design website";
 	$email_body = "User Name: $name.\n".
 	               "User Email: $visitor_email.\n".
 				   "User Message: $message.\n";
 	
-	$to= "muland@steve-musans.info";
+	$to= "info@inspiration-design.org";
 	$headers  = "From: $email_form \r\n";
 	$headers .= "Reply-To: $visitor_email \r\n";
 	
 	mail($to, $email_subject, $email_body, $headers);
 	
-	header("Location: index.html");
+	header("Location: home.html");
 
 ?>
